5e35f0810273f6f81d5467ad13285d81f9b3af9f
Vojtěch Mareš <vojtech@mares.cz>
2026-04-03 18:01:17 +0200

    feat: initialize shadcn ui components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig, fontProviders } from "astro/config";
+import {defineConfig, fontProviders} from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://wiki.example.com",
+  site: "https://wiki.mares.cz",
   output: "static",
   integrations: [react(), sitemap()],
   vite: {
